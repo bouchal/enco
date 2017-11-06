60dd863ce56dffee5cea08a755ac7fdd7fcaa5e9
scorpio1337 <me@justme.cz>
2017-11-06 15:31:00 +0100

    Fix JsonLoader name

--- src/lib/JsonLoader.js
+++ src/lib/JsonLoader.js
@@ -2,10 +2,10 @@ import json from 'jsonfile';
 
 import AbstractFileLoader from './AbstractFileLoader';
 
-class CsonLoader extends AbstractFileLoader {
+class JsonLoader extends AbstractFileLoader {
     _loadConfig(file) {
         return json.readFileSync(file)
     }
 }
 
-export default CsonLoader;
+export default JsonLoader;
